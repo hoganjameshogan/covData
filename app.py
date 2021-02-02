d7344fbb2c9cc87b3f04368ce9c4d5eea5194a1f
James Hogan <jameshoganobair@gmail.com>
2021-02-02 15:16:35 +0100

    update map display

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 import folium
 from folium.map import FeatureGroup
+from folium.plugins import MarkerCluster
+
 from geopy.geocoders import ArcGIS
 import webbrowser
 import pandas as pd
 from datetime import datetime, timedelta
+
+load = open("world.json", "r",encoding="utf-8-sig").read()
+
+fg = folium.FeatureGroup(name="foo")
+
+worldMap = folium.Map(tiles="cartodbpositron")
+
+
+fg = folium.FeatureGroup(name="foo")
+fg.add_child(folium.GeoJson(data=load))
+worldMap.add_child(fg)
+
+worldMap.save("test.html")
+
+webbrowser.open("test.html")
 
 data = pd.read_json(r"cov_02-02-21.json")
 # test2 = pd.DataFrame(test)
@@ -18,5 +35,5 @@ for item in data:
             # print(dt)
             # print(res['date'])
         if res['date'] == dtf:
-            outStr = "%s : %s" % (data[item]['location'],res['new_cases'])
+            outStr = "%s : %s" % (data[item]['location'],int(res['new_cases']))
             print(outStr)
