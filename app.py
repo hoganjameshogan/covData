fb4e078ee82fdb5a1a4f950327642813a17a057f
James Hogan <jameshoganobair@gmail.com>
2021-02-02 13:29:07 +0100

    iterate through data

--- app.py
+++ app.py
@@ -2,3 +2,22 @@ import folium
 from folium.map import FeatureGroup
 from geopy.geocoders import ArcGIS
 import webbrowser
+import pandas as pd
+from datetime import datetime, timedelta
+
+test = pd.read_json(r"cov_02-02-21.json")
+test2 = pd.DataFrame(test)
+
+dt = (datetime.today().now() - timedelta(days=1))
+dtf = dt.strftime("%Y-%m-%d")
+
+for item in test:
+    if test[item]['location'] == "Ireland" :
+        # print(test[item]['data'][0]['date'])
+        for res in test[item]['data']:
+            # print(dt)
+            # print(res['date'])
+            if res['date'] == dtf:
+                print(dt)
+                outStr = "%s : %s" % (dtf,res['new_cases'])
+                print(outStr)
