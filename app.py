5042c6f11feb94618ba9c7ab66393a2d5435e635
James Hogan <jameshoganobair@gmail.com>
2021-02-02 13:36:30 +0100

    formatting update

--- app.py
+++ app.py
@@ -5,19 +5,18 @@ import webbrowser
 import pandas as pd
 from datetime import datetime, timedelta
 
-test = pd.read_json(r"cov_02-02-21.json")
-test2 = pd.DataFrame(test)
+data = pd.read_json(r"cov_02-02-21.json")
+# test2 = pd.DataFrame(test)
 
 dt = (datetime.today().now() - timedelta(days=1))
 dtf = dt.strftime("%Y-%m-%d")
 
-for item in test:
-    if test[item]['location'] == "Ireland" :
+for item in data:
+    
         # print(test[item]['data'][0]['date'])
-        for res in test[item]['data']:
+    for res in data[item]['data']:
             # print(dt)
             # print(res['date'])
-            if res['date'] == dtf:
-                print(dt)
-                outStr = "%s : %s" % (dtf,res['new_cases'])
-                print(outStr)
+        if res['date'] == dtf:
+            outStr = "%s : %s" % (data[item]['location'],res['new_cases'])
+            print(outStr)
